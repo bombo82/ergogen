625b93913399786cf74b34990d5a6cc7ea45db3b
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2024-08-28 23:37:46 +0200

    make Kailh PG1350 reversible and generally improve the footprint

--- src/footprints/choc.js
+++ src/footprints/choc.js
@@ -5,25 +5,28 @@
 // Params
 //    hotswap: default is false
 //      if true, will include holes and pads for Kailh choc hotswap sockets
-//    reverse: default is false
+//    hotswap_thruhole: default is false
+//      if true, will remove the thru hole for solder directly the switch
+//    reversible: default is false
 //      if true, will flip the footprint such that the pcb can be reversible
 //    keycaps: default is false
 //      if true, will add choc sized keycap box around the footprint
-// 
-// note: hotswap and reverse can be used simultaneously
+//
+// note: hotswap and reversible can be used simultaneously
 
 module.exports = {
   params: {
     designator: 'S',
     hotswap: false,
-    reverse: false,
+    hotswap_thruhole: false,
+    reversible: false,
     keycaps: false,
     from: undefined,
     to: undefined
   },
   body: p => {
     const standard = `
-      (module PG1350 (layer F.Cu) (tedit 5DD50112)
+      module PG1350 (layer F.Cu) (tedit 5DD50112)
       ${p.at /* parametric position */}
 
       ${'' /* footprint reference */}
@@ -38,11 +41,11 @@ module.exports = {
       (fp_line (start 7 6) (end 7 7) (layer Dwgs.User) (width 0.15))
       (fp_line (start 7 -7) (end 6 -7) (layer Dwgs.User) (width 0.15))
       (fp_line (start 6 7) (end 7 7) (layer Dwgs.User) (width 0.15))
-      (fp_line (start 7 -7) (end 7 -6) (layer Dwgs.User) (width 0.15))      
-      
+      (fp_line (start 7 -7) (end 7 -6) (layer Dwgs.User) (width 0.15))
+
       ${''/* middle shaft */}
       (pad "" np_thru_hole circle (at 0 0) (size 3.429 3.429) (drill 3.429) (layers *.Cu *.Mask))
-        
+
       ${''/* stabilizers */}
       (pad "" np_thru_hole circle (at 5.5 0) (size 1.7018 1.7018) (drill 1.7018) (layers *.Cu *.Mask))
       (pad "" np_thru_hole circle (at -5.5 0) (size 1.7018 1.7018) (drill 1.7018) (layers *.Cu *.Mask))
@@ -54,38 +57,47 @@ module.exports = {
       (fp_line (start 9 8.5) (end -9 8.5) (layer Dwgs.User) (width 0.15))
       (fp_line (start -9 8.5) (end -9 -8.5) (layer Dwgs.User) (width 0.15))
       `
-    function pins(def_neg, def_pos, def_side) {
-      if(p.hotswap) {
+
+    function pins() {
+      if (p.hotswap) {
         return `
           ${'' /* holes */}
-          (pad "" np_thru_hole circle (at ${def_pos}5 -3.75) (size 3 3) (drill 3) (layers *.Cu *.Mask))
+          (pad "" np_thru_hole circle (at 5 -3.75) (size 3 3) (drill 3) (layers *.Cu *.Mask))
           (pad "" np_thru_hole circle (at 0 -5.95) (size 3 3) (drill 3) (layers *.Cu *.Mask))
-      
+          (pad "" np_thru_hole circle (at 0 0) (size 3.429 3.429) (drill 3.429) (layers *.Cu *.Mask))
+          ${p.reversible ? '(pad "" np_thru_hole circle (at -5 -3.75) (size 3 3) (drill 3) (layers *.Cu *.Mask))' : ''}
+          (pad "" np_thru_hole circle (at 5.5 0) (size 1.7018 1.7018) (drill 1.7018) (layers *.Cu *.Mask))
+          (pad "" np_thru_hole circle (at -5.5 0) (size 1.7018 1.7018) (drill 1.7018) (layers *.Cu *.Mask))
+
           ${'' /* net pads */}
-          (pad 1 smd rect (at ${def_neg}3.275 -5.95 ${p.r}) (size 2.6 2.6) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask)  ${p.from})
-          (pad 2 smd rect (at ${def_pos}8.275 -3.75 ${p.r}) (size 2.6 2.6) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask)  ${p.to})
+          ${p.reversible ?
+            `(pad 1 smd roundrect (at -3.275 -5.95) (size 2.6 2.6) (layers B.Cu B.Paste B.Mask) (roundrect_rratio 0) (chamfer_ratio 0.5) (chamfer bottom_left) ${p.from})`
+            :
+            `(pad 1 smd rect (at -3.275 -5.95) (size 2.6 2.6) (layers B.Cu B.Paste B.Mask)${p.from})`
+          }
+          ${p.reversible ? `(pad 1 smd roundrect (at 3.275 -5.95) (size 2.6 2.6) (layers F.Cu F.Paste F.Mask) (roundrect_rratio 0) (chamfer_ratio 0.5) (chamfer bottom_right) ${p.from})` : ''}
+          (pad 2 smd rect (at 8.275 -3.75) (size 2.6 2.6) (layers B.Cu B.Paste B.Mask) ${p.to})
+          ${p.reversible ? `(pad 2 smd rect (at -8.275 -3.75) (size 2.6 2.6) (layers F.Cu F.Paste F.Mask) ${p.to})` : ''}
+
+          ${''/* pins */}
+          ${p.hotswap_thruhole ? `(pad 1 thru_hole circle (at 0 5.9) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.from})` : ''}
+          ${p.reversible && p.hotswap_thruhole ? `(pad 2 thru_hole circle (at 5 3.8) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.to})` : ''}
+          ${p.hotswap_thruhole ? `(pad 2 thru_hole circle (at -5 3.8) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.to})` : ''}
         `
       } else {
-          return `
-            ${''/* pins */}
-            (pad 1 thru_hole circle (at ${def_pos}5 -3.8) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.from})
-            (pad 2 thru_hole circle (at ${def_pos}0 -5.9) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.to})
-          `
+        return `
+          ${''/* pins */}
+          (pad 1 thru_hole circle (at 0 -5.9) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.from})
+          (pad 2 thru_hole circle (at 5 -3.8) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.to})
+          ${p.reversible ? `(pad 2 thru_hole circle (at -5 -3.8) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.to})` : ''}
+        `
       }
     }
-    if(p.reverse) {
-      return `
-        ${standard}
-        ${p.keycaps ? keycap : ''}
-        ${pins('-', '', 'B')}
-        ${pins('', '-', 'F')})
-        `
-    } else {
-      return `
-        ${standard}
-        ${p.keycaps ? keycap : ''}
-        ${pins('-', '', 'B')})
-        `
-    }
+
+    return `(
+      ${standard}
+      ${p.keycaps ? keycap : ''}
+      ${pins()}
+    )`
   }
 }
